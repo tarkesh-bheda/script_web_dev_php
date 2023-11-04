947ea61ab38538800acd45098a00fa97a4055274
tarkesh-bheda <143687205+tarkesh-bheda@users.noreply.github.com>
2023-11-04 09:23:21 +0530

    Add files via upload

---
+++ Array_Ex/array_12.php
@@ -0,0 +1,9 @@
+<?php
+//12. Create an associative array representing a person's details, including name, age, and email.
+
+$personal_detail = array(
+    "name"=>"mihir",
+    "age"=>"21",
+    "email"=>"abc@gmail.com");
+print_r($personal_detail);
+?>
